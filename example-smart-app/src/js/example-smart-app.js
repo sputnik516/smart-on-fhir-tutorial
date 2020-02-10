0721e9fbe9f433bfdd866bb77c972e38a265c6bd
Alex Lazurka <23346931+sputnik516@users.noreply.github.com>
2020-02-09 22:29:26 -0500

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -17,7 +17,9 @@
                       code: {
                         $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
                               'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
+                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4',
+                              'http://loinc.org|29463-7'
+                             ]
                       }
                     }
                   });
@@ -37,6 +39,7 @@
           }
 
           var height = byCodes('8302-2');
+          var weight = byCodes('29463-7');
           var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
           var hdl = byCodes('2085-9');
@@ -44,6 +47,7 @@
 
           var p = defaultPatient();
           p.birthdate = patient.birthDate;
+          p.weight = weight;
           p.gender = gender;
           p.fname = fname;
           p.lname = lname;
